305dae54837684bd9613b2c411de3b9a1570e781
89769 <75208228+karan-65@users.noreply.github.com>
2022-01-14 15:10:43 +0530

    find middle of linkedlist

---
+++ src/com/company/linkedlist/findMiddleOfll.java
@@ -0,0 +1,39 @@
+package com.company.linkedlist;
+//finding middle of linkedlist using the concept of slow and fast node
+
+
+public class findMiddleOfll {
+    public Node middle(Node head){
+        Node fast=head;
+        Node slow =head;
+        while(fast!=null||fast.next!=null){
+            //first slow will move one step
+            slow=slow.next;
+            //fast will move two steps
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+
+
+
+    private class Node{
+
+        private int value;
+        private Node next;
+
+        public Node(){
+
+        }
+
+        public Node(int value, Node next){
+            this.value=value;
+            this.next=next;
+        }
+}
+
+
+    public static void main(String[] args) {
+
+    }
+}
