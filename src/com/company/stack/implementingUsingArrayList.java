13fbcbd181d3e4def08b3ffb221b8147de2a723d
89769 <75208228+karan-65@users.noreply.github.com>
2022-01-17 20:01:17 +0530

    implementing stack using arrayList

---
+++ src/com/company/stack/implementingUsingArrayList.java
@@ -0,0 +1,41 @@
+package com.company.stack;
+
+import java.util.ArrayList;
+
+public class implementingUsingArrayList {
+    public static class Stack{
+        ArrayList<Integer> list=new ArrayList<>();
+        public  boolean isEmpty(){
+            return list.size()==0;
+
+        }
+        public void push(int data){
+                list.add(data);
+            }
+
+        public int pop(){
+            int top= list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+
+        public int peek(){
+            return list.get(list.size()-1);
+        }
+
+
+
+
+    }
+    public static void main(String[] args) {
+        Stack s= new Stack();
+        s.push(2);
+        s.push(3);
+        s.push(4);
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+    }
+}
