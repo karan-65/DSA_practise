4a1626b6563ca8a0a95d2e90196c9c1b570f57f0
89769 <75208228+karan-65@users.noreply.github.com>
2022-01-31 23:14:48 +0530

    pre order traversal

---
+++ src/com/company/binaryTree/binaryTreeRepres.java
@@ -0,0 +1,48 @@
+package com.company.binaryTree;
+
+public class binaryTreeRepres {
+    Node root;
+    binaryTreeRepres() {
+        root = null;
+    }
+    public void preorder(Node node){
+
+        if(node==null){
+            return;
+        }
+        System.out.println(node.data);
+        preorder(node.left);
+        preorder(node.right);
+    }
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        public Node(int key){
+            this.data=key;
+        }
+
+
+    }
+    public static void main(String[] args) {
+//        Node n=new Node(2);
+//        n.left=new Node(1);
+//        n.right=new Node (3);
+//        n.right.right=new Node(5);
+        binaryTreeRepres t=new binaryTreeRepres();
+        t.root=new Node(1);
+        t.root.left=new Node(2);
+        t.root.right=new Node(3);
+        t.root.left.left = new Node(5);
+        t.root.left.right = new Node(6);
+        t.preorder(t.root);
+
+
+
+
+
+
+
+
+    }
+}
