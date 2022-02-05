48f34e21d51535c51b42562773a299becea6471c
89769 <75208228+karan-65@users.noreply.github.com>
2022-02-05 16:37:59 +0530

    balanced binary tree

---
+++ src/com/company/binaryTree/balancedBinaryTree.java
@@ -0,0 +1,44 @@
+package com.company.binaryTree;
+
+public class balancedBinaryTree {
+
+        public static int balancedbinary(Node node){
+            if(node==null){
+                return 0;
+            }
+            int leftheight=balancedbinary(node.left);
+            if(leftheight==-1){
+                return -1;
+            }
+            int rightheight=balancedbinary(node.right);
+            if(rightheight==-1){
+                return -1;
+            }
+            if(Math.abs(leftheight-rightheight)>1){
+                return -1;
+            }
+            return Math.max(leftheight,rightheight)+1;
+
+
+    }
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static void main(String[] args) {
+        Node node =new Node(1);
+        node.left=new Node(2);
+        node.right=new Node(3);
+        node.left.left = new Node(4);
+        node.left.right = new Node(5);
+        node.right.left = new Node(6);
+        System.out.println(balancedbinary(node));
+
+
+    }
+}
