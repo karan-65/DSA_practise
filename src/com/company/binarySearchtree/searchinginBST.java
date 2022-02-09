eb5eacf29750a6e846c0192b9707695095b223e3
89769 <75208228+karan-65@users.noreply.github.com>
2022-02-09 15:31:16 +0530

    searching in binary tree

---
+++ src/com/company/binarySearchtree/searchinginBST.java
@@ -0,0 +1,32 @@
+package com.company.binarySearchtree;
+
+public class searchinginBST {
+    public static class Node{
+        int data;
+        Node left=null;
+        Node right=null;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static Node search(Node root,int val){
+        {
+            if(root==null || root.data== val)
+                return root;
+            if(root.data > val)
+                return search(root.left, val);
+            return search(root.right, val);
+        }
+    }
+    public static void main(String[] args) {
+        Node root=new Node(8);
+        root.left=new Node(3);
+        root.right=new Node(10);
+        root.left.left=new Node(1);
+        root.left.right=new Node(6);
+        root.right.left=new Node(9);
+        root.right.right=new Node(14);
+        root.right.right.right=new Node(18);
+        System.out.println(search(root,10));
+    }
+}
