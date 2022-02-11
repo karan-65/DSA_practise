b3fa5ed9b4e1fbb7e27da8fedee8ba63e8be068b
89769 <75208228+karan-65@users.noreply.github.com>
2022-02-11 15:44:24 +0530

    inserting in bst

---
+++ src/com/company/binarySearchtree/insertinginBST.java
@@ -0,0 +1,43 @@
+package com.company.binarySearchtree;
+
+public class insertinginBST {
+    public class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public Node insert(Node node,int val) {
+        if (node == null) {
+            return new Node(val);
+        }
+        Node curr = node;
+        while (true) {
+            if (curr.data <= val) {
+                if (curr.right != null) {
+                    curr = curr.right;
+                } else {
+                    curr.right = new Node(val);
+                    break;
+                }
+            } else {
+                if (curr.left != null) {
+                    curr = curr.left;
+                } else {
+                    curr.left = new Node(val);
+                    break;
+                }
+            }
+
+        }
+        return node;
+
+        }
+
+
+    public static void main(String[] args) {
+
+    }
+}
