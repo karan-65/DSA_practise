425e0fd3e13f23d5a0e57b2b04141b6c9b07a677
89769 <75208228+karan-65@users.noreply.github.com>
2022-02-17 18:18:31 +0530

    construct bst from preorder

---
+++ src/com/company/binarySearchtree/constructingFromPreorder.java
@@ -0,0 +1,28 @@
+package com.company.binarySearchtree;
+
+public class constructingFromPreorder {
+    public class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    int i=0;
+    public Node bstFromPreorder(int[]arr){
+        return fun(arr,Integer.MAX_VALUE);
+    }
+    public Node fun(int[]arr,int right){
+        if(i==arr.length||arr[i]>right){
+            return null;
+        }
+        Node node=new Node(arr[i++]);
+        node.left=fun(arr,node.data);
+        node.right=fun(arr,right);
+        return node;
+    }
+    public static void main(String[] args) {
+
+    }
+}
