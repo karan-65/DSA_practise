f614c0f86c4565ba4dcb8a7fad44da2e2f9fbf72
89769 <75208228+karan-65@users.noreply.github.com>
2022-07-06 16:55:22 +0530

    ceiling number

--- src/com/company/binarysearch/ceilingnumberleetcode.java
+++ src/com/company/binarysearch/ceilingnumberleetcode.java
@@ -1,6 +1,7 @@
 package com.company.binarysearch;
 
 public class ceilingnumberleetcode {
+    //binary search algo
     public static int binarysearch(int []arr,int target){
         int start=arr[0];
         int end=arr.length-1;
@@ -19,9 +20,30 @@ public class ceilingnumberleetcode {
     }
         return -1;
     }
+    public static int cieling(int []arr,int target){
+        //ceiling means the smallest number greater from target
+        int start=arr[0];
+        int end=arr.length-1;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>target){
+                end=mid-1;
+            }
+            else if (arr[mid]<target){
+                start=mid+1;
+            }
+            else{
+                return mid;
+            }
+        }
+        //here is the only change from binary search
+
+        return start;
+    }
     public static void main(String[] args) {
-        int[] arr={-5,-3,-1,0,3,4,7,9};
-        int ans=binarysearch(arr,5);
+        int[] arr={2,3,5,9,14,16,19};
+        int ans=cieling(arr,15);
         System.out.println(ans);
 
 
