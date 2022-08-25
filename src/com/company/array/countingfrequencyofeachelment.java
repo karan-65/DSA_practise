e3be95fc3ce228b40c26786c79142e3f914d4110
89769 <75208228+karan-65@users.noreply.github.com>
2022-08-25 21:53:24 +0530

    calcualting frequency of each element

---
+++ src/com/company/array/countingfrequencyofeachelment.java
@@ -0,0 +1,32 @@
+package com.company.array;
+//here using the approach of already visited element using two for loops
+
+public class countingfrequencyofeachelment {
+    public static void count(int[]arr,int n){
+//this array here is to check the true values that will be true if they are already visited
+        boolean visited[]=new boolean[n];
+        for(int i=0;i<n;i++){
+            //skipping the element that is already visited
+            if(visited[i]==true){
+                continue;
+            }
+            //initalising the count for counting
+            int count=1;
+            //second loop comparing the value with all other values
+            for(int j=i+1;j<n;j++){
+                if(arr[i]==arr[j]){
+                    //setting visited equals to true if if it found the same value
+                    visited[j]=true;
+                    count++;
+                }
+            }
+            System.out.println(arr[i]+ " "+count);
+        }
+
+    }
+    public static void main(String[] args) {
+        int[]arr={2,3,4,5,2,2,2,3,3,4,4,5};
+        count(arr,12);
+
+    }
+}
