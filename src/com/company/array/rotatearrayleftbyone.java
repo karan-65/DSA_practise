d1230aac368fe09c3a8e1b100c31832762ca0e81
89769 <75208228+karan-65@users.noreply.github.com>
2022-08-27 16:18:18 +0530

    rotate array to left by one

---
+++ src/com/company/array/rotatearrayleftbyone.java
@@ -0,0 +1,21 @@
+package com.company.array;
+
+import java.util.Arrays;
+//so basically we store the first element and then pushed every element backward and then assinged temp to last
+public class rotatearrayleftbyone {
+    public static void rotate(int[]arr,int n){
+        //stored the first element here
+        int temp=arr[0];
+        //pushed every element backward
+        for(int i=0;i<arr.length-1;i++){
+            arr[i]=arr[i+1];
+        }
+        //and assinged first elemnt to last
+        arr[n-1]=temp;
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        rotate(arr,5);
+    }
+}
