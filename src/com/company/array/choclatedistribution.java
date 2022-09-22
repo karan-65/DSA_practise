9b30123a5e35e5a2a8d56757fcdb302055658163
89769 <75208228+karan-65@users.noreply.github.com>
2022-09-22 22:45:50 +0530

    choclatedistribution

---
+++ src/com/company/array/choclatedistribution.java
@@ -0,0 +1,28 @@
+package com.company.array;
+
+import java.util.Arrays;
+
+public class choclatedistribution {
+    public static void fun(int []arr,int m,int n){
+        Arrays.sort(arr);
+
+        int ans=Integer.MAX_VALUE;
+        for(int i=0;i<n-m;i++){
+            int min=arr[i];
+            int max=arr[i+m-1];
+            int diff=max-min;
+            if(diff<ans){
+                ans=diff;
+            }
+
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {7, 3, 2, 4, 9, 12, 56};
+        fun(arr,3,7);
+
+    }
+}
