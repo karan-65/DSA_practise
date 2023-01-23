5968abe091597483e0230ebf2b7eec7d8473a371
89769 <75208228+karan-65@users.noreply.github.com>
2023-01-23 13:56:28 +0530

    sum 0f 2 row

--- src/com/company/twodarray/printthenumber0f7.java
+++ src/com/company/twodarray/printthenumber0f7.java
@@ -1,21 +1,29 @@
 package com.company.twodarray;
 
 public class printthenumber0f7 {
-    public static void fun(int[][]arr){
-        int count =0;
-        for(int i=0;i<arr.length;i++){
-            for(int j=0;j<arr[0].length;j++){
-                if(arr[i][j]==7){
-                    count++;
-                }
-            }
-        }
-        System.out.println(count);
+//    public static void fun(int[][]arr){
+//        int count =0;
+//        for(int i=0;i<arr.length;i++){
+//            for(int j=0;j<arr[0].length;j++){
+//                if(arr[i][j]==7){
+//                    count++;
+//                }
+//            }
+//        }
+//        System.out.println(count);
+//
+//    }
 
+    public static void fun(int[][]arr){
+        int sum=0;
+        for(int i=0;i<arr[0].length;i++){
+                    sum=sum+arr[1][i];
+            }
+        System.out.println(sum);
     }
 
     public static void main(String[] args) {
-        int arr[][]={{1,2,3},{4,5,7},{7,8,9}};
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
         fun(arr);
-    }
+}
 }
