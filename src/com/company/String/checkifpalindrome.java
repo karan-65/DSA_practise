7fe54f2b9d130d6cba2237655f77f4d695f5ccec
89769 <75208228+karan-65@users.noreply.github.com>
2023-01-23 23:05:03 +0530

    to check palindrome string

---
+++ src/com/company/String/checkifpalindrome.java
@@ -0,0 +1,22 @@
+package com.company.String;
+
+public class checkifpalindrome {
+    public static boolean fun(String str){
+        int i=0;
+        int j=str.length()-1;
+        while(i<j){
+            if(str.charAt(i)!=str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str="lail";
+        System.out.println(fun(str));
+    }
+
+}
