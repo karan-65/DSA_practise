5fc93ab86524dbd62c857e37a64561c276ff7eba
89769 <75208228+karan-65@users.noreply.github.com>
2023-02-02 21:31:40 +0530

    subarraysumequaltoK

--- src/com/company/hashMap/subarraysumequaltoK.java
+++ src/com/company/hashMap/subarraysumequaltoK.java
@@ -1,5 +1,7 @@
 package com.company.hashMap;
 
+import java.util.HashMap;
+
 public class subarraysumequaltoK {
 
     //this one is brute force using two for loops and calulating the count
@@ -17,8 +19,26 @@ public class subarraysumequaltoK {
 //        }
 //        System.out.println(count);
 //    }
+
+    //bhai note book dekh or video bhi bhenchod concept he yeh
     public static void main(String[] args) {
         int arr[]={1,2,3};
+        int k=3;
+
+        HashMap<Integer,Integer>hm=new HashMap<>();
+        hm.put(0,1);
+        int ans=0;
+        int sum=0;
+        for(int j=0;j<arr.length;j++){
+            sum+=arr[j];
+            if(hm.containsKey(sum-k)){
+                ans+=hm.get(sum-k);
+            }
+
+                hm.put(sum,hm.getOrDefault(sum,0)+1);
+
+        }
+        System.out.println(ans);
 
     }
 }
