7746272f29b170b1a5160bcdf4ca32c14117c95f
89769 <75208228+karan-65@users.noreply.github.com>
2023-02-02 20:11:59 +0530

    subarraysumequaltoK

---
+++ src/com/company/hashMap/subarraysumequaltoK.java
@@ -0,0 +1,24 @@
+package com.company.hashMap;
+
+public class subarraysumequaltoK {
+
+    //this one is brute force using two for loops and calulating the count
+//    public static void fun(int[]arr){
+//        int count=0;
+//
+//        for(int i=0;i<arr.length;i++){
+//            int sum=0;
+//            for(int j=i;j<arr.length;j++){
+//                sum=sum+arr[j];
+//                if(sum==3){
+//                    count++;
+//                }
+//            }
+//        }
+//        System.out.println(count);
+//    }
+    public static void main(String[] args) {
+        int arr[]={1,2,3};
+
+    }
+}
