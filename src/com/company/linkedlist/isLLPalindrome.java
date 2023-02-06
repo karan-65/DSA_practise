da4a3175e68e1246031862a8af3a11a5ace703fe
89769 <75208228+karan-65@users.noreply.github.com>
2023-02-06 21:18:42 +0530

    ll palindrome again fck

--- src/com/company/linkedlist/isLLPalindrome.java
+++ src/com/company/linkedlist/isLLPalindrome.java
@@ -1,46 +1,82 @@
 package com.company.linkedlist;
 
 public class isLLPalindrome {
-    public boolean fun(Node head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
-        //first find middle of the linked list
-        Node fast = head;
-        Node slow = head;
-        while (fast != null && fast.next.next != null) {
-            slow = slow.next;
-            fast = fast.next;
-        }
-        //reversing the other half of the linked list
-        //slow here refer to the middle
-        slow.next = reversell(slow.next);
+//    public boolean fun(Node head) {
+//        if (head == null || head.next == null) {
+//            return true;
+//        }
+//        //first find middle of the linked list
+//        Node fast = head;
+//        Node slow = head;
+//        while (fast != null && fast.next.next != null) {
+//            slow = slow.next;
+//            fast = fast.next;
+//        }
+//        //reversing the other half of the linked list
+//        //slow here refer to the middle
+//        slow.next = reversell(slow.next);
+//
+//        //slow will refer to the first of the other reversed part
+//        slow = slow.next;
+//        //comparing the other half with the first half if equal it will return true
+//        while (slow != null) {
+//            if (head.value != slow.value) {
+//                return false;
+//            }
+//            head = head.next;
+//            slow = slow.next;
+//
+//        }
+//        return true;
+//    }
+//
+//        public Node reversell(Node head){
+//            Node temp=null;
+//            while(head!=null){
+//                Node next=head.next;
+//                head.next=temp;
+//                temp=head;
+//                head=next;
+//            }
+//            return temp;
+//        }
+//
 
-        //slow will refer to the first of the other reversed part
-        slow = slow.next;
-        //comparing the other half with the first half if equal it will return true
-        while (slow != null) {
-            if (head.value != slow.value) {
+    public static Node mid(Node head){
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+
+        }
+        return slow;
+    }
+
+    public static boolean reverse(Node head){
+        Node mid=mid(head);
+
+        Node prev=null;
+        Node curr=mid;
+        Node next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        Node right=prev;
+        Node left=head;
+
+        while(right!=null){
+            if(left.value!=right.value){
                 return false;
             }
-            head = head.next;
-            slow = slow.next;
-
+            left=left.next;
+            right=right.next;
         }
         return true;
     }
-
-        public Node reversell(Node head){
-            Node temp=null;
-            while(head!=null){
-                Node next=head.next;
-                head.next=temp;
-                temp=head;
-                head=next;
-            }
-            return temp;
-        }
-
 
 
     private class Node{
