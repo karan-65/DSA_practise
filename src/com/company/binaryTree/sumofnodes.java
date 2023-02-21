c4142f333b7c7d7787ced3c333a3d9fa1ea87e03
89769 <75208228+karan-65@users.noreply.github.com>
2023-02-21 12:57:39 +0530

    sum of nodes

---
+++ src/com/company/binaryTree/sumofnodes.java
@@ -0,0 +1,32 @@
+package com.company.binaryTree;
+
+public class sumofnodes {
+    public static class Node{
+        int data;
+        Node right;
+        Node left;
+         Node(int data){
+             this.data=data;
+             this.right=null;
+             this.left=null;
+         }
+    }
+
+    public static int fun(Node root){
+        if(root==null){
+            return 0;
+        }
+        int left=fun(root.left);
+        int right=fun(root.right);
+        int sum=root.data+left+right;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Node root=new Node(1);
+        root.left=new Node(3);
+        root.right=new Node(4);
+        root.left.right=new Node(6);
+        System.out.println(fun(root));
+    }
+}
